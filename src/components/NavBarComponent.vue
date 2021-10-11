3502fe46e5dea357aa10fe45cc7e624d33cc22ef
MatejKomlossy <matej.komlossy@gmail.com>
2021-10-11 16:17:43 +0200

    simple registration
    Signed-off-by: MatejKomlossy <matej.komlossy@gmail.com>

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/NotFound">Not Found</router-link> |
-    <router-link to="/Login">Login</router-link>
+    <router-link to="/Login">Login</router-link> |
+    <router-link to="/Registration">Registration</router-link>
   </div>
 
 
