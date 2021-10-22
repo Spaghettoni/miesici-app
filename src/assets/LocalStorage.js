f1ed6bf509845d5e428b5926df2a41dba1bc5bed
Spaghettoni <kunzo6@gmail.com>
2021-10-22 11:32:55 +0200

    create events entry point
    Signed-off-by: Spaghettoni <kunzo6@gmail.com>

--- src/assets/LocalStorage.js
+++ src/assets/LocalStorage.js
@@ -1,20 +1,55 @@
 const storage = {
-  "users": [
-    {"username": "Janko", "password": "Janko", "mail":"janko@gmail.com"},
+    "users": [
+        {
+            "username": "Janko",
+            "password": "Janko",
+            "mail": "janko@gmail.com"
+        },
 
-    {"username": "Peter", "password": "Peter", "mail":"peter@gmail.com"},
+        {
+            "username": "Peter",
+            "password": "Peter",
+            "mail": "peter@gmail.com"
+        },
 
-    {"username": "Ondrej", "password": "Ondrej", "mail":"ondrej@gmail.com"}
-  ],
+        {
+            "username": "Ondrej",
+            "password": "Ondrej",
+            "mail": "ondrej@gmail.com"
+        }
+    ],
 
-  "loggedUser": null,
+    "loggedUser": null,
 
-  "teams": [
-    {"name": "Kolegovci", "members": ["Janko"]},
-    {"name": "Miesici", "members": ["Janko"]},
-    {"name": "Futbalisti", "members": ["Peter", "Ondrej"]}
-  ],
+    "teams": [
+        {
+            "name": "Kolegovci",
+            "members": ["Janko"]
+        },
+        {
+            "name": "Miesici",
+            "members": ["Janko"]
+        },
+        {
+            "name": "Futbalisti",
+            "members": ["Peter", "Ondrej"]
+        }
+    ],
 
+    "events": [
+        {
+            "name": "Veľký šampionát v Bernolákove",
+            "team": "Miesici",
+        },
+        {
+            "name": "DL Boyz derby",
+            "team": "Miesici",
+        },
+        {
+            "name": "Račiansky playoff",
+            "team": "Futbalisti",
+        },
+    ],
 
 }
 
---
+++ src/components/events/EventsPageComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="w-full flex justify-center">
+    <div class="mt-10 p-4 flex flex-col w-full max-w-3xl border-2">
+      <div class="cursor-pointer font-semibold hover:text-orange px-5 py-3 border max-w-min"
+           @click="goBack"
+      >
+        &lt;&nbsp;back
+      </div>
+      <div class="mt-6 flex justify-between items-center">
+        <h1 class="mb-4 font-semibold text-6xl">
+          My Upcoming Events
+        </h1>
+        <router-link
+            to="/create-team"
+            class="ml-14 flex border items-center hover:bg-orange cursor-pointer">
+          <img alt="" :src="'/pictures/create.png'" width="50">
+          <div class="font-semibold pr-4">
+            Create new event
+          </div>
+        </router-link>
+      </div>
+      <div class="flex flex-col">
+        <router-link class="mt-6 border cursor-pointer hover:shadow-xl hover:bg-orange"
+                     v-bind:key=team v-for="team in this.teams"
+                     :to="{name: 'TeamDetail', params: {teamName: team.name, members: team.members}}">
+          <team-component
+              :team-name=team.name
+              :members=team.members
+          ></team-component>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TeamsController from "../../controllers/TeamsController";
+import router from "../../router";
+
+export default {
+  name: "EventsComponent",
+  data() {
+    return {
+      events: null,
+    }
+  },
+  methods: {
+    goBack() {
+      router.back()
+    }
+  },
+  mounted() {
+    this.events = TeamsController.getTeams();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
