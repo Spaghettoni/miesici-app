743961843e8f3af754fa5849d28da96d4e3b04a0
boborova3 <jan.boborova@gmail.com>
2021-11-27 20:15:15 +0100

    mobile version adjusted (not finished)

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,27 +1,83 @@
 <template>
   <div id="header">
-    <div @click="updatePath('/')"
-         class="mx-10 float-left cursor-pointer" v-if="activePage !== '/'">
-      <img alt="" :src="'/pictures/icon2.png'" width="100">
+    <div class="flex flex-col">
+      <div>
+          <div @click="updatePath('/')"
+               class="ml-10 float-left cursor-pointer" v-if="activePage !== '/'">
+            <img alt="" :src="'/pictures/icon2.png'" width="91">
 
-    </div>
-    <div
-         class="mx-10 float-left" v-else>
-      <img alt="" :src="'/pictures/icon2.png'" width="100">
+          </div>
+          <div
+               class="ml-10 float-left" v-else>
+            <img alt="" :src="'/pictures/icon2.png'" width="91">
 
-    </div>
+          </div>
 
-    <nav id="nav"
-         class="float-left flex justify-end sm:px-0 sm:flex-row sm:justify-end py-4 sm:py-4 font-semibold">
-<!--      <div @click="updatePath('/')"-->
-<!--           class="button-40 mx-1.5 hidden sm:block"-->
-<!--           v-bind:class="{active: activePage === '/'}"-->
-<!--      >-->
-<!--        Home-->
-<!--      </div>-->
+          <nav id="nav"
+               class="float-left flex ml-5 justify-end sm:px-0 sm:flex-row sm:justify-end py-4 sm:py-4 font-semibold">
+      <!--      <div @click="updatePath('/')"-->
+      <!--           class="button-40 mx-1.5 hidden sm:block"-->
+      <!--           v-bind:class="{active: activePage === '/'}"-->
+      <!--      >-->
+      <!--        Home-->
+      <!--      </div>-->
+            <div v-if="this.state.loggedUser !== null"
+                 @click="updatePath('/teams')"
+                 class="button-40 mx-1.5 hidden sm:block"
+                 v-bind:class="{active: activePage === '/teams'}"
+            >
+              <i class="fas fa-users" style="font-size:16px; margin-right: 5px"></i>
+              Teams
+            </div>
+            <div v-if="this.state.loggedUser !== null"
+                 @click="updatePath('/events')"
+                 class="button-40 mx-1.5 hidden sm:block"
+                 v-bind:class="{active: activePage === '/events'}"
+            >
+              <i class="far fa-calendar-alt" style="font-size:16px; margin-right: 5px"></i>
+              Events
+            </div>
+
+          </nav>
+
+          <div class="float-right flex flex-row justify-end mr-5 sm:px-0 sm:flex-row sm:justify-end py-4 sm:py-4 font-semibold">
+            <div  v-if="this.state.loggedUser === null"
+                  @click="updatePath('/login')"
+                  class="button-40 mx-1.5"
+                  v-bind:class="{active: activePage === '/login'}"
+            >
+      <!--        <i class="fas fa-sign-in-alt" style="font-size:16px"></i>-->
+                      Login
+            </div>
+                  <div  v-if="this.state.loggedUser === null"
+                        @click="updatePath('/register')"
+                        class="sign-up-button-40 mx-1.5"
+                        v-bind:class="{active: activePage === '/register'}"
+                  >
+                    Register
+                  </div>
+            <div v-if="this.state.loggedUser !== null"
+                 @click="updatePath('/profile')"
+                 class="button-40 ml-10 mx-1.5"
+                 v-bind:class="{active: activePage === '/profile'}"
+            >
+              <i class="fas fa-user-alt" style="font-size:16px"></i>
+      <!--                Profile-->
+            </div>
+            <div  v-if="this.state.loggedUser !== null"
+                  @click="this.logout()"
+                  class="logout-button-40 mx-1.5"
+            >
+              <i class="fas fa-sign-out-alt" style="font-size:16px"></i>
+      <!--                Logout-->
+            </div>
+          </div>
+        </div>
+
+    <div class="bottom-navbar self-end flex flex-row sm:hidden">
       <div v-if="this.state.loggedUser !== null"
            @click="updatePath('/teams')"
-           class="button-40 mx-1.5 hidden sm:block"
+           class="mobile-button-40"
            v-bind:class="{active: activePage === '/teams'}"
       >
         <i class="fas fa-users" style="font-size:16px; margin-right: 5px"></i>
@@ -29,104 +85,15 @@
       </div>
       <div v-if="this.state.loggedUser !== null"
            @click="updatePath('/events')"
-           class="button-40 mx-1.5 hidden sm:block"
+           class="mobile-button-40"
            v-bind:class="{active: activePage === '/events'}"
       >
         <i class="far fa-calendar-alt" style="font-size:16px; margin-right: 5px"></i>
         Events
       </div>
-
-      <div
-          class="mx-4 border-2 py-2 px-7 text-center cursor-pointer hover:bg-orange flex-shrink sm:hidden font-bold select-none"
-          @click="toggleNav"
-      >
-        =
-      </div>
-      <div class="burgir absolute top-16 py-4 flex flex-grow w-full flex-col sm:hidden bg-white border-2"
-           v-if="this.showMenu"
-      >
-        <div @click="updatePath('/')"
-             class="mx-4 border-2 py-2 px-5 text-center cursor-pointer bg-white hover:bg-orange relative"
-             v-bind:class="{active2: activePage === '/'}"
-        >
-          Home
-        </div>
-        <div v-if="this.state.loggedUser !== null"
-             @click="updatePath('/profile')"
-             class="mx-4 border-2 py-2 px-5 text-center cursor-pointer bg-white hover:bg-orange relative"
-             v-bind:class="{active2: activePage === '/profile'}"
-        >
-          Profile
-        </div>
-        <div v-if="this.state.loggedUser !== null"
-             @click="updatePath('/teams')"
-             class="mx-4 border-2 py-2 px-5 text-center cursor-pointer bg-white hover:bg-orange"
-             v-bind:class="{active2: activePage === '/teams'}"
-        >
-          Teams
-        </div>
-        <div v-if="this.state.loggedUser !== null"
-             @click="updatePath('/events')"
-             class="mx-4 border-2 py-2 px-5 text-center cursor-pointer hover:bg-orange "
-             v-bind:class="{active2: activePage === '/events'}"
-        >
-          Events
-        </div>
-        <div  v-if="this.state.loggedUser === null"
-              @click="updatePath('/login')"
-              class="mx-4 border-2 py-2 px-5 text-center cursor-pointer hover:bg-orange"
-              v-bind:class="{active2: activePage === '/login'}"
-        >
-          Login
-        </div>
-        <div  v-if="this.state.loggedUser === null"
-              @click="updatePath('/register')"
-              class="mx-4 border-2 py-2 px-5 text-center cursor-pointer hover:bg-orange"
-              v-bind:class="{active2: activePage === '/register'}"
-        >
-          Register
-        </div>
-        <div  v-if="this.state.loggedUser !== null"
-              @click="this.logout()"
-              class="mx-4 border-2 py-2 px-5 text-center cursor-pointer hover:bg-orange"
-        >
-          Logout
-        </div>
-      </div>
-    </nav>
-
-    <div class="float-right flex justify-end mr-5 sm:px-0 sm:flex-row sm:justify-end py-4 sm:py-4 font-semibold">
-      <div  v-if="this.state.loggedUser === null"
-            @click="updatePath('/login')"
-            class="button-40 mx-1.5 hidden sm:block"
-            v-bind:class="{active: activePage === '/login'}"
-      >
-<!--        <i class="fas fa-sign-in-alt" style="font-size:16px"></i>-->
-                Login
-      </div>
-            <div  v-if="this.state.loggedUser === null"
-                  @click="updatePath('/register')"
-                  class="sign-up-button-40 mx-1.5 hidden  sm:block"
-                  v-bind:class="{active: activePage === '/register'}"
-            >
-              Register
-            </div>
-      <div v-if="this.state.loggedUser !== null"
-           @click="updatePath('/profile')"
-           class="button-40 ml-10 mx-1.5 hidden sm:block"
-           v-bind:class="{active: activePage === '/profile'}"
-      >
-        <i class="fas fa-user-alt" style="font-size:16px"></i>
-<!--                Profile-->
-      </div>
-      <div  v-if="this.state.loggedUser !== null"
-            @click="this.logout()"
-            class="logout-button-40 mx-1.5 hidden sm:block"
-      >
-        <i class="fas fa-sign-out-alt" style="font-size:16px"></i>
-<!--                Logout-->
-      </div>
     </div>
+    </div>
+
   </div>
 </template>
 
@@ -158,9 +125,6 @@ export default {
       //TODO problem po login -> nezobrazi ako aktivnu stranku Upcoming events
       this.activePage = target;
     },
-    toggleNav() {
-      this.showMenu = !this.showMenu;
-    },
     logout() {
       LoginController.logout();
       this.activePage = '/';
@@ -174,10 +138,6 @@ export default {
 </script>
 
 <style scoped>
-.burgir {
-  box-shadow: 0 40px 50px rgba(0, 0, 0, 0.80);
-  color: black;
-}
 
 #header {
   box-sizing: border-box;
@@ -218,6 +178,39 @@ export default {
 }
 
 .button-40:focus {
+  box-shadow: none;
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+}
+
+.mobile-button-40 {
+  background-color: #111827;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  flex: 0 0 auto;
+  font-family: "Inter var",ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  font-size: 1.000rem;
+  font-weight: 600;
+  line-height: 1.5rem;
+  padding: .45rem 1.2rem;
+  text-align: center;
+  text-decoration: none #6B7280 solid;
+  text-decoration-thickness: auto;
+  transition-duration: .2s;
+  transition-property: background-color,border-color,color,fill,stroke;
+  transition-timing-function: cubic-bezier(.4, 0, 0.2, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 50%;
+}
+
+.mobile-button-40:hover {
+  background-color: orange;
+}
+
+.mobile-button-40:focus {
   box-shadow: none;
   outline: 2px solid transparent;
   outline-offset: 2px;
@@ -306,9 +299,9 @@ export default {
   color: black;
 }
 
-.active2 {
-  background-color: black;
-  color: white;
+.bottom-navbar {
+  background-color: #333;
+  width: 100%;
 }
 
 </style>
