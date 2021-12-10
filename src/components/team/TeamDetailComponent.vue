1b0cb6ba01ba1b72902e7d3c40167a267de73075
MatejKomlossy <matej.komlossy@gmail.com>
2021-12-10 13:53:47 +0100

    do stlpca brah
    Signed-off-by: MatejKomlossy <matej.komlossy@gmail.com>

--- src/components/team/TeamDetailComponent.vue
+++ src/components/team/TeamDetailComponent.vue
@@ -1,45 +1,46 @@
 <template>
   <div class="w-full flex justify-center">
-    <div class="mt-10 px-4 flex flex-col w-full max-w-3xl">
-      <back-button></back-button>
-      <div>
-        <h1 class="mb-4 font-semibold text-6xl">
-          {{ this.team.name }}
-        </h1>
-      </div>
-      <div class="flex flex-shrink">
-        <div class="flex flex-col">
-          <div class="flex flex-wrap font-semibold text-lg">
-            <span class="font-bold"> members: &nbsp;</span>
-            <div class="mr-2"
-                 v-bind:key=username v-for="username in this.memberNames()">
-              {{ username }},
+    <div class="mt-10 px-4 flex flex-wrap flex-col w-full max-w-3xl sm:flex-row">
+      <div class="flex flex-col">
+        <back-button></back-button>
+        <div>
+          <h1 class="mb-4 font-semibold text-6xl">
+            {{ this.team.name }}
+          </h1>
+        </div>
 
+        <div class="mt-5">
+          <div class="text-lg italic">
+            Add new member:
+          </div>
+          <div class="flex flex-col sm:flex-row">
+            <input type="text" id="newMember" name="member"
+                  v-model="input.member"
+                  placeholder="Tomero"
+                  class="px-4 py-2 text-xl border-2 border-black max-w-sm rounded"
+                  list="allUsernames"
+                  autocomplete="on"
+            />
+            <div class="sm:ml-4 mt-4 sm:mt-0 text-center max-w-min">
+              <button class="h-full w-full px-6 py-2 border-black bg-orange
+                            rounded-xl font-semibold text-lg hover:shadow-xl
+                              hover-zoom"
+                      @click="addMember"
+              >
+                Add
+              </button>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="mt-5">
-        <div class="text-lg italic">
-          Add new member:
-        </div>
-        <div class="flex flex-col sm:flex-row">
-          <input type="text" id="newMember" name="member"
-                 v-model="input.member"
-                 placeholder="Tomero"
-                 class="px-4 py-2 text-xl border-2 border-black max-w-sm rounded"
-                 list="allUsernames"
-                 autocomplete="on"
-          />
-          <div class="sm:ml-4 mt-4 sm:mt-0 text-center max-w-min">
-            <button class="h-full w-full px-6 py-2 border-black bg-orange
-                           rounded-xl font-semibold text-lg hover:shadow-xl
-                            hover-zoom"
-                    @click="addMember"
-            >
-              Add
-            </button>
+      <div class="flex flex-shrink mt-10 sm:mt-0 sm:ml-10 sm:border-l-2 sm:pl-10">
+        <div class="flex flex-col font-semibold text-lg">
+          <span class="font-bold"> members: &nbsp;</span>
+          <div class="mr-2"
+              v-bind:key=username v-for="username in this.memberNames()">
+            {{ username }}
+
           </div>
         </div>
       </div>
